cd69a3ec44a47157102c83c5f8e47ea102b32405
Badzela <yasher2000@mail.ru>
2018-11-03 02:23:00 +0300

    Задания нормал к 5 уроку. Плюс новый файл с функциями к этому уроку

--- lesson_05/home_work/hw05_normal.py
+++ lesson_05/home_work/hw05_normal.py
@@ -13,8 +13,31 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+import hw05_easy_for_normal as f
+print("Меню опреаций")
+print("1. Перейти в папку")
+print("2. Просмотреть содержимое текущей папки")
+print("3. Удалить папку")
+print("4. Создать папку")
 
+answer = int(input("Выберите номер опреации, которую вы хотите выполнить"))
 
+if answer == 1:
+    operation_ans = input("Введите название папки, в которую хотите перейти")
+    f.change_dir(operation_ans)
+
+elif answer == 2:
+    f.dir_view()
+
+elif answer == 3:
+    operation_ans = input("Введите название папки, которую хотите удалить")
+    f.dir_delete(operation_ans)
+
+elif answer == 4:
+    operation_ans = input("Введите название папки, которую хотите создать")
+    f.dir_make(operation_ans)
+else:
+    print("Вы выбрали некорректную операцию")
 
 
 
